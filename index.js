001a7df5fbf03e18fe73ef52538532fc3f4f44fb
Denis <den_nesterov_1992@mail.ru>
2022-09-09 16:40:16 +0500

    Add new categories

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function Render() {
         let valueTotal = 0;
         itemArr.forEach(function(item) {
             if (item.incomeExpenses === 'Income') {
-                return valueTotal += Number.parseFloat(item.amount);
+                valueTotal += Number.parseFloat(item.amount);
             }
             if (item.incomeExpenses === 'Expenses') {
-                return valueTotal -= Number.parseFloat(item.amount);
+                valueTotal -= Number.parseFloat(item.amount);
             }
             return valueTotal;
         })
@@ -33,12 +33,12 @@ function Render() {
 
 // Создаем конструктор пункта расхода/дохода
 class Item {
-    constructor (incomeExpenses, amount, categories, date, tag) {
+    constructor (incomeExpenses, amount, categories, date, comment) {
         this.incomeExpenses = incomeExpenses,
         this.amount = amount,
         this.categories = categories,
         this.date = date,
-        this.tag = tag;
+        this.comment = comment;
     }
 
     // Макет пункта для рендера
@@ -49,10 +49,44 @@ class Item {
                 <div>${this.amount}</div>
                 <div>${this.categories}</div>
                 <div>${this.date}</div>
-                <div>${this.tag}</div>
+                <div>${this.comment}</div>
             </div>`
     }
 }
+
+// Объект со свойствами доход/расход
+class IncomeOrExpenses {
+
+    // Объект с категориями дохода
+    income = {
+        salary: 'Salary',
+        pension: 'Pension',
+        socialPayments: 'Social payments',
+        donations: 'Donations',
+        sellingThings: 'Selling things',
+    }
+
+    // Объект с категориями расхода
+    expenses = {
+        automobile: 'Automobile',
+        house: 'House',
+        health: 'Health',
+        personalExpenses: 'Personal expenses',
+        clothing: 'Сlothing',
+        food: 'Food',
+        present: 'Present',
+        familyExpenses: 'Family expenses',
+        technique: 'Technique',
+        services: 'Services',
+        utilities: 'Utilities',
+    }
+}
+
+function doRenderCategories(item) {
+        return `<option>${item}</option>`
+}
+
+const incomeOrExpenses = new IncomeOrExpenses();
 
 // Находим форму с данными пункта
 const formEl = document.querySelector('form');
@@ -75,10 +109,10 @@ document.addEventListener('click', event => {
         let amount = document.querySelector('[data-amount]').value;
         let categories = document.querySelector('[data-categories]').value;
         let date = document.querySelector('[data-date]').value;
-        let tag = document.querySelector('[data-tag]').value;
+        let comment = document.querySelector('[data-comment]').value;
 
         // Создаем новый пункт расхода/дохода на основе данных из формы и помещаем его в массив
-        itemArr.push(new Item(incomeExpenses, amount, categories, date, tag));
+        itemArr.push(new Item(incomeExpenses, amount, categories, date, comment));
 
         // Выводим массив в консоль (для удобства разработки)
         console.log(itemArr);
@@ -87,3 +121,28 @@ document.addEventListener('click', event => {
         Render();
     }
 })
+
+// Ищем поле выбора доход/расход
+const dataIncomeExpenses = document.querySelector('[data-incomeExpenses]');
+
+const dataCategories = document.querySelector('[data-categories]');
+// Отслеживаем выбора
+dataIncomeExpenses.addEventListener('input', function() {
+
+    if (dataIncomeExpenses.value === '') {
+        dataCategories.innerHTML = null;
+    }
+    // Проверяем доход или расход
+    if (dataIncomeExpenses.value === 'Expenses') {
+
+        let arr = Object.values(incomeOrExpenses.expenses);
+        dataCategories.innerHTML = arr.map(item => 
+            doRenderCategories(item)).join('');
+    }
+    if (dataIncomeExpenses.value === 'Income') {
+        
+        let arr = Object.values(incomeOrExpenses.income);
+        dataCategories.innerHTML = arr.map(item => 
+            doRenderCategories(item)).join('');
+    }
+});
